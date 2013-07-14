35051dbe06354878b4e06de01f1af10594ab318e
Hemant Gupta <rajni.hemant@gmail.com>
2013-07-14 17:32:03 +0530

    we

---
+++ MyExamples/CallableFutureExample/src/MainTest.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+public class MainTest implements Callable<String>{
+	
+	@Override
+    public String call() throws Exception {
+        Thread.sleep(3000);
+        return Thread.currentThread().getName();
+    }
+     
+    public static void main(String args[]){
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        //create a list to hold the Future object associated with Callable
+        List<Future<String>> list = new ArrayList<Future<String>>();
+
+        Callable<String> callable = new MainTest();
+        for(int i=0; i< 100; i++){
+            Future<String> future = executor.submit(callable);
+            list.add(future);
+        }
+        for(Future<String> fut : list){
+            try {
+                //print the return value of Future, notice the output delay in console
+                // because Future.get() waits for task to get completed
+                System.out.println(new Date()+ "::"+fut.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        //shut down the executor service now
+        executor.shutdown();
+    }
+}
